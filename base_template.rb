d058ecee94a3abd3d5c9f2fcdd4aa15673f2e9ba
David McNally <david.mcnally@dsl.pipex.com>
2009-05-07 20:24:12 +0100

    Added factory_girl to template

--- base_template.rb
+++ base_template.rb
@@ -1,12 +1,16 @@
 # base_template.rb
 run "echo TODO > README"
 
+gem "rspec", :version => ">=1.1.99.8"
+gem "rspec-rails", :version => ">=1.1.99.8"
+gem "aslakhellesoy-cucumber", :version => ">= 1.1.99.21", :source => "http://gems.github.com"
+gem "webrat", :version => ">= 0.4.2"
+gem "thoughtbot-factory_girl", :lib => "factory_girl",:source => "http://gems.github.com"
+
+plugin "attachment_fu", :git => "git://github.com/technoweenie/attachment_fu.git"
 plugin "restful_authentication", :git => "git://github.com/technoweenie/restful-authentication.git"
-plugin "rspec", :git => "git://github.com/dchelimsky/rspec.git"
-plugin "rspec-rails", :git => "git://github.com/dchelimsky/rspec-rails.git"
 
 generate :rspec
-
 generate :authenticated, "user sessions"
 
 git :init
